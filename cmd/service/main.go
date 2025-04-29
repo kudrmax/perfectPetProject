9e7b8fbfed6b4a19e3a3cf571a8f65c93a0a7822
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-04-30 01:45:50 +0300

    ADD main

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"my/perfectPetProject/internal/repositories/postgres/posts_repo"
+	"my/perfectPetProject/internal/repositories/postgres/users_repo"
+	"my/perfectPetProject/internal/services/posts"
+)
 
+func main() {
+	userRepository := users_repo.NewRepository()
+	postRepository := posts_repo.NewRepository()
+	postService := posts.NewService(postRepository, userRepository)
+	_ = postService
 }
