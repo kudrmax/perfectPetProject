c054263e9373ad300fb4e4234d7caddb822f3396
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-05-01 15:06:42 +0300

    ADD server to main

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"my/perfectPetProject/internal/repositories/postgres/posts_repo"
-	"my/perfectPetProject/internal/repositories/postgres/users_repo"
-	"my/perfectPetProject/internal/services/posts"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/oapi-codegen/nethttp-middleware"
+
+	"github.com/kudrmax/perfectPetProject/internal/api"
+	"github.com/kudrmax/perfectPetProject/internal/handlers"
+	"github.com/kudrmax/perfectPetProject/internal/repositories/postgres/posts_repository"
+	"github.com/kudrmax/perfectPetProject/internal/repositories/postgres/users_repository"
+	"github.com/kudrmax/perfectPetProject/internal/services/posts"
 )
 
 func main() {
-	userRepository := users_repo.NewRepository()
-	postRepository := posts_repo.NewRepository()
-	postService := posts.NewService(postRepository, userRepository)
-	_ = postService
+	userRepository := users_repository.NewRepository()
+	postRepository := posts_repository.NewRepository()
+	postService := posts.NewService(
+		postRepository,
+		userRepository,
+	)
+
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		log.Fatalf("❌ failed to load swagger: %v", err)
+	}
+	swagger.Servers = nil
+
+	router := chi.NewRouter()
+	router.Use(nethttpmiddleware.OapiRequestValidator(swagger)) // валидация API
+
+	handler := handlers.NewHandler(postService)
+	router.Mount("/", api.Handler(handler))
+
+	log.Println("Server started at http://localhost:8080")
+	if err = http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("❌ server exited with error: %v", err)
+	}
 }
