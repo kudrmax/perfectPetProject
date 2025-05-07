b8af45d5cf6157e9319d31323903100a77b6bb4d
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-05-07 22:01:44 +0300

    Remove redoc handler

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/mvrilo/go-redoc"
 
 	myHttp "github.com/kudrmax/perfectPetProject/internal/handlers/http"
 	"github.com/kudrmax/perfectPetProject/internal/handlers/http/api"
@@ -22,13 +21,9 @@ import (
 func main() {
 	rootRouter := chi.NewRouter()
 
-	rootRouter.Mount("/docs", getRedocRouter())
 	rootRouter.Mount("/", getApiRouter())
 
 	log.Println("Server started at http://localhost:8080")
-	log.Println("API: http://localhost:8080/api/1/tweets")
-	log.Println("Feed: http://localhost:8080/api/1/tweets/feed")
-	log.Println("OpenAPI docs at http://localhost:8080/docs/openapi")
 	if err := http.ListenAndServe(":8080", rootRouter); err != nil {
 		log.Fatalf("❌ server exited with error: %v", err)
 	}
@@ -80,29 +75,4 @@ func getApiRouter() http.Handler {
 	router.Mount("/", api.Handler(server))
 
 	return router
-}
-
-func getRedocRouter() http.Handler {
-	doc := redoc.Redoc{
-		Title:       "API Documentation",
-		Description: "Интерактивная документация для API",
-		SpecFile:    "./openapi/openapi.gen.yaml",
-		SpecPath:    "/docs/openapi.yaml", // относительный путь внутри /docs
-		DocsPath:    "/docs/openapi",      // будет доступен как /docs/openapi
-	}
-
-	router := chi.NewRouter()
-
-	router.Get("/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, doc.SpecFile)
-	})
-	router.Get("/openapi", doc.Handler())
-
-	return router
-}
-
-func getSwaggerRouter() http.Handler {
-	// использовать гайд отсюда:
-	// https://youtu.be/87au30fl5e4?t=1376
-	return nil
 }
